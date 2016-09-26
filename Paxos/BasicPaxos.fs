ab19c7dd30b3878d2e419ac370e364684b7338e4
Steffen <stj@danskebank.dk>
2016-09-26 19:10:03 +0200

    ...

--- Paxos/BasicPaxos.fs
+++ Paxos/BasicPaxos.fs
@@ -71,6 +71,7 @@ module BasicPaxos =
 // retry in proposer
 // handle at least once delivery (or argue that we dont need it)
 // timeout
+// force reset (in case of too many acceptors failing. We can get stuck in an in-between state)
   
   type N = int
   type Value = string
@@ -149,15 +150,22 @@ module BasicPaxos =
   let proposerReceiveFromAcceptor (quorumSize:int) (s:PState) (m:AMsg) : (PState * (Destination * PMsg) option) =
     let ignore = (s,None)
     //for now, we will push the action of retrying to the client.
-    let nackClient s cr reason = 
-      let ((guid,sender),_,_) = cr
-      in (s, Some (Client sender, MClientNack (guid, reason)))
-    let nackPrepare cr n n' newN =
+//    let nackClient s cr reason = 
+//      let ((guid,sender),_,_) = cr
+//      in (s, Some (Client sender, MClientNack (guid, reason)))
+//    let nackPrepare cr n n' newN =
+//      if (n = n')
+//      then if(newN > n)
+//           then nackClient (PReady newN) cr "proposer behind" //The nack proposes a newer n. It does not matter if it was a response to this session directly. Abort.
+//           else nackClient s cr "proposer behind"
+//      else ignore //Different session (old, since it is a response) => Ignore. 
+    let retry ((_,k,_) as cr:ClientRequest) n n' proposedN = 
       if (n = n')
-      then if(newN > n)
-            then nackClient (PReady newN) cr "proposer behind" //The nack proposes a newer n. It does not matter if it was a response to this session directly. Abort.
-            else nackClient s cr "proposer behind"
-      else ignore //Different session (old, since it is a response) => Ignore. 
+      then if (proposedN > n)
+           then let retryN = proposedN + 1
+                in (PPrepareSent (retryN,cr,[]), Some (BroadcastAcceptors, MPrepare (retryN,k)))
+           else ignore
+      else ignore
     match s with
     | PReady n -> //This session is initiated by another proposer
       match m with
@@ -177,11 +185,9 @@ module BasicPaxos =
                   let v' = f maxV
                   in (PAcceptSent(n, cr, []), Some (BroadcastAcceptors, MAccept(n,cs,k,v'))) //send accept
              else (PPrepareSent(n, cr, promises'), None) //wait for more promises
-        else if (n < n')
-             then nackClient (PReady n') cr "proposer behind" //if it is newer we cancel session and up the n to the newest
-             else ignore //it was an old one. Ignore
+        else ignore //a promise can never be newer, since it is a response. So it must be an old one
       | MAccepted _ -> ignore //Old => Ignore
-      | MNackPrepare (n',newN) -> nackPrepare cr n n' newN
+      | MNackPrepare (n',newN) -> retry cr n n' newN
     | PAcceptSent (n, cr, accepts) ->
       match m with
       | MPromise _ -> ignore //either from another session, or a promise that was not needed for this one. Ignore
@@ -191,10 +197,11 @@ module BasicPaxos =
              if (isQuorum quorumSize accepts')
              then (PReady n, None) //session done, assume learner will answer client.
              else (PAcceptSent (n,cr,accepts'), None) //collect accept
-        else if (n < n')
-             then nackClient (PReady n') cr "proposer behind" //if it is newer we cancel session and up the n to the newest
-             else ignore //it was an old one. Ignore
-      | MNackPrepare (n',newN) -> nackPrepare cr n n' newN
+        else ignore 
+        //even if this is a newer one from another session, we are forced to ignore it while collecting our responses. Otherwise we will risk losing a db update
+        //this is probably the reason we need a force reset (in case less than quorum accepter are available)
+
+      | MNackPrepare (n',newN) -> retry cr n n' newN
 
 
   //note that the client is modelled as running locally on the proposer 
@@ -223,7 +230,7 @@ module BasicPaxos =
     | MAccept (n',cs, k',v') ->
       if (n <= n') 
       then let store' = Map.add k' (n',v') store
-           in (AReady (n', store'), Some (Broadcast, MAccepted (n, cs, k', v')))
+           in (AReady (n', store'), Some (Broadcast, MAccepted (n, cs, k', v'))) //TODO maybe dont send to other proposers
       else (AReady (n, store), Some (Proposer sender, MNackPrepare (n',n)))
     | MClientNack _ -> ignore //not relevant for acceptor
 
