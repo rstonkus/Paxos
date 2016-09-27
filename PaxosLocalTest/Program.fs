d79f915558fa8c83248cf2dae4873d2fc2f5f499
Steffen <stj@danskebank.dk>
2016-09-27 15:48:51 +0200

    Must model an external client in order to test more than 1 req pr proposer.

--- PaxosLocalTest/Program.fs
+++ PaxosLocalTest/Program.fs
@@ -18,26 +18,32 @@ let clusterOf3 () =
     |];
   in (quorumSize,participants)
 
-[<Property(MaxTest = 500)>]
+//a simple mutator that always writes "v1"
+let writeV1 vvo = "v1"
+
+//read with default
+let readWithDefault d vvo = 
+  match vvo with
+  | None -> d
+  | Some (_,v) -> v
+
+let increment vvo = 
+  match vvo with
+  | None -> "0"
+  | Some (_,v) -> let i = System.Int32.Parse v
+                  in (i + 1).ToString()
+
+
+[<Property(MaxTest = 100)>]
 let ``single round write and readwithDefault`` (seed:int) = 
   let debug = false
   let (quorumSize, participants) = clusterOf3 ()
-  let random = System.Random(seed)//1204047459 -> default
-
-  //a simple mutator that always writes "v1"
-  let writeV1 vvo = "v1"
-
-  //read with default
-  let readWithDefault d vvo = 
-    match vvo with
-    | None -> d
-    | Some (_,v) -> v
+  let random = System.Random(seed)
 
   let result = { Run.Responses = [] }
   let client1 = Participant.findClient "client1" participants
   let client2 = Participant.findClient "client2" participants
   
-
 
   let guid1 = System.Guid.NewGuid()
   let req1 = (Proposer "proposer1", CMsg (MClientRequest ((guid1,"client1"),"key1", writeV1)))
@@ -46,9 +52,9 @@ let ``single round write and readwithDefault`` (seed:int) =
   let guid2 = System.Guid.NewGuid()
   let req2 = (Proposer "proposer2", CMsg (MClientRequest ((guid2,"client2"),"key1", readWithDefault "default")))
   let () = Run.clientSend client2 req2
+  
+
   let () = Run.runToEnd debug random quorumSize participants result
-
-
 
   //verify
   let v1s = Run.Verify.responsesToGuid guid1 result
@@ -60,13 +66,55 @@ let ``single round write and readwithDefault`` (seed:int) =
   let allEq1 = Seq.forall ((=) v1) v1s
   let allEq2 = Seq.forall ((=) v2) v2s
   
-  //let () = printfn "%A" result.Responses
-
   let always = v1 = "v1" && allEq1 && allEq2
   let case1 = v2 = "default"
   let case2 = v2 = "v1"
   always && (case1 || case2)
 
+
+[<Property(MaxTest = 100)>]
+let ``1000 increments by two clients`` (seed:int) = 
+  let debug = false
+  let (quorumSize, participants) = clusterOf3 ()
+  let random = System.Random(seed)
+
+  let result = { Run.Responses = [] }
+  let client1 = Participant.findClient "client1" participants
+  let client2 = Participant.findClient "client2" participants
+  
+  let twoIncs () = 
+    let guid1 = System.Guid.NewGuid()
+    let req1 = (Proposer "proposer1", CMsg (MClientRequest ((guid1,"client1"),"key1", increment)))
+    let () = Run.clientSend client1 req1
+
+    let guid2 = System.Guid.NewGuid()
+    let req2 = (Proposer "proposer2", CMsg (MClientRequest ((guid2,"client2"),"key1", increment)))
+    let () = Run.clientSend client2 req2
+    ()
+  
+  let bs = 
+    seq {
+      for i in 1 .. 1000 -> 
+        twoIncs ()
+    }
+  let () = Seq.iter id bs
+
+  let () = Run.runToEnd debug random quorumSize participants result
+
+  let g = System.Guid.NewGuid()
+  let r = (Proposer "proposer1", CMsg (MClientRequest ((g,"client1"),"key1", readWithDefault "default")))
+  let () = Run.clientSend client1 r
+  let () = Run.runToEnd debug random quorumSize participants result
+
+
+  //verify
+  let vs = Run.Verify.responsesToGuid g result
+
+  let v = Seq.head vs //there must be one anyway
+  let allEq = Seq.forall ((=) v) vs
+  
+  let always = v = "1999" && allEq
+  always
   
 
 
